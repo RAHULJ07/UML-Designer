b6da1f8ca661ef7d1ce18d7f59ea05c2c99a6365
chrislazar25 <89318462+chrislazar25@users.noreply.github.com>
2022-11-25 16:52:33 -0700

    Update StatusBar.java

--- src/StatusBar.java
+++ src/StatusBar.java
@@ -2,13 +2,23 @@ import javax.swing.*;
 import java.awt.Color;
 
 public class StatusBar extends JLabel {
-  public StatusBar(){
-    super();
-    super.setBorder(BorderFactory.createLineBorder(Color.black));
-    setMessage("Status:");
-  }
   
-  public void setStatus(String message){
-    setText(" "+message);
-  }
+	protected static StatusBar instance = null;
+	
+	protected StatusBar(){
+	  super();
+	  super.setBorder(BorderFactory.createLineBorder(Color.black));
+	  setText("Status:");
+	  }
+	  
+	  public static StatusBar getStatus() {
+		  if (instance == null) {
+			  instance = new StatusBar();
+		  }
+		  return instance;
+	  }
+	  
+	  public void setStatus(String message){
+	    setText(" "+message);
+	  }
 }
